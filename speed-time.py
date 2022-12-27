93b61faed953d96b46466cc5e092f2d6dfc17c55
Serohon <74919179+Serohon@users.noreply.github.com>
2022-12-27 12:45:46 +0300

    Add files via upload
    График зависимости скорости ракеты от времени полёта

---
+++ speed-time.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+
+fig, ax = plt.subplots()
+
+x = (60, 120, 180, 240, 300, 360)
+y = (210, 842, 1573, 1978, 2644, 2712)
+
+plt.title('Зависимость скорости от времени до орбиты Земли')
+plt.xlabel('Время, с')
+plt.ylabel('Скорость, м/c')
+
+ax.plot(x, y, color='grey', linestyle='-', marker='o')
+plt.show()
